e22392bfc2b0c4bcd8d88e0aa6df31e9276ba35f
Emma Connolly <emconnolly72@gmail.com>
2024-07-20 16:37:50 -0400

    Added error handling to create.sh

--- create.sh
+++ create.sh
@@ -3,6 +3,22 @@
 # Creates a script file with a pre-added shebang
 
 FILENAME=$1
+if [ -z $FILENAME ]
+then
+	echo "Please supply a file name."
+	exit 1
+fi
+
+if [ -f $FILENAME ]
+then
+	echo $FILENAME + " already exists. Would you like to replace it?"
+	read -r REPLACE
+	if [ $REPLACE -ne "[Yy]*" ]
+	then
+		exit 1
+	fi
+fi
+
 touch $FILENAME
 chmod 755 $FILENAME
 echo "#!/usr/bin/env bash" >> $FILENAME
